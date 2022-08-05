5c6bd2939988f95bce66375a6d775022e5636d5d
Git-Yee <git-yee@ZipcodersMBP58.fios-router.home>
2022-08-05 13:42:31 -0400

    updated students

--- src/main/java/io/zipcoder/interfaces/Students.java
+++ src/main/java/io/zipcoder/interfaces/Students.java
@@ -14,27 +14,4 @@ public class Students extends People<Student> { //Singleton
         return instance;
     }
 
-    public static void main(String[] args) {
-//        public int[] twoSum(int[] nums, int target) {
-
-            int [] nums = new int[]{2,7,11,15};
-            int target = 9;
-
-            int temp1 = 0;
-            int temp2 = 0;
-            for (int i = 0; i < nums.length; i++) {
-
-                for (int j = 0; j < nums.length; j++) {
-                    if (nums[i] + nums[j] == target)
-                        temp1 = i;
-                    temp2 = j;
-                }
-
-            }
-            int[] indices = new int[2];
-            indices[0] = temp1;
-            indices[1] = temp2;
-
-        System.out.println(Arrays.toString(indices));
-    }
 }
